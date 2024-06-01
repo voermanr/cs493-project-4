8c0e427b7969966b8a0014865d5890ffcdaa6d73
Ross Voerman <voermanr@oregonstate.edu>
2024-06-01 13:45:20 -0700

    Adding route for thumbnail getting

--- api/media.js
+++ api/media.js
@@ -4,35 +4,38 @@ const router = require('express').Router();
 
 exports.router = router
 
+async function sendImageFromBucket(imageId, bucketName, req, res, next) {
+    try {
+        const gridFSBucket = new GridFSBucket(getDB(), {
+            bucketName: bucketName,
+        });
+
+        const files = await gridFSBucket.find({
+            _id: imageId
+        }).toArray();
+
+        const file = files[0];
+
+        if (file && file.length > 0 && file.metadata.contentType.split('/')[1] === req.params.ext) {
+            res.type(file.metadata.contentType)
+            const gridFSBucketReadStream = gridFSBucket.openDownloadStreamByName(file.filename);
+            gridFSBucketReadStream.pipe(res)
+        } else {
+            res.status(404).json({error: "Photo not phound for download!"});
+        }
+    } catch (e) {
+        next(e);
+    }
+}
+
 router.get('/photos/:photoid.:ext', async (req, res, next) => {
     if (req.params.ext === 'png' || req.params.ext === 'jpg') {
         const photoid = await new ObjectId(req.params.photoid)
-
-        try {
-            const gridFSBucket = new GridFSBucket(getDB(), {
-                bucketName: 'images',
-            });
-
-            const files = await gridFSBucket.find({
-                _id: photoid
-            }).toArray();
-
-            const file = files[0];
-            //console.log('file:', file)
-            //console.log('file.metadata:', file.metadata)
-
-            if (file && file.length > 0 && file.metadata.contentType.split('/')[1] === req.params.ext) {
-                res.type(file.metadata.contentType)
-                const gridFSBucketReadStream = gridFSBucket.openDownloadStreamByName(file.filename);
-                //console.log('gridFSBucketReadStream:', gridFSBucketReadStream);
-                gridFSBucketReadStream.pipe(res)
-            }
-            else {
-                res.status(404).json( {error: "Photo not phound for download!"});
-            }
-        }
-        catch (e) {
-            next(e);
-        }
+        await sendImageFromBucket(photoid, 'images', req, res, next);
     }
 })
+
+router.get('/media/thumbnails/:thumbid.jpg', async (req, res, next) => {
+    const thumbId = new ObjectId(req.params.thumbid);
+    await sendImageFromBucket(thumbId, 'thumbnails', req, res, next)
+});
